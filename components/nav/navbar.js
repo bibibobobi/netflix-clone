7e5455b4991b7e0a396636862dd925bf6e70ec49
bibibobobi <smilingcat33@gmail.com>
2022-06-16 15:36:24 +0800

    navbar routing

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,18 +1,38 @@
 import styles from './navbar.module.css';
 
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 const NavBar = (props) => {
   const { username } = props;
+
+  const router = useRouter();
+
+  const handleOnClickHome = (e) => {
+    e.preventDefault();
+    router.push('/');
+  };
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push('/browse/my-list');
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <a className={styles.logoLink} href='/'>
-          <div className={styles.logoWrapper}>Netflix</div>
-        </a>
+        <Link href='/'>
+          <a className={styles.logoLink}>
+            <div className={styles.logoWrapper}>Netflix</div>
+          </a>
+        </Link>
 
         <ul className={styles.navItems}>
-          <li className={styles.navItem}>Home</li>
-          <li className={styles.navItem2}>My List</li>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
         </ul>
         <nav className={styles.navContainer}>
           <div>
@@ -23,7 +43,9 @@ const NavBar = (props) => {
 
             <div className={styles.navDropdown}>
               <div>
-                <a className={styles.linkName}>Sign out</a>
+                <Link href='/login'>
+                  <a className={styles.linkName}>Sign out</a>
+                </Link>
                 <div className={styles.lineWrapper}></div>
               </div>
             </div>
