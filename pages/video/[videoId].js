f81620adb410ec38052734c9f76d98ad0cf3745b
bibibobobi <smilingcat33@gmail.com>
2022-06-30 19:57:05 +0800

    modal styling

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -2,10 +2,23 @@ import { useRouter } from 'next/router';
 import Modal from 'react-modal';
 import styles from '../../styles/Video.module.css';
 
+import cls from 'classnames';
+
 Modal.setAppElement('#__next');
 
 const Video = () => {
   const router = useRouter();
+
+  const video = {
+    title: 'SPY x FAMILY',
+    publishTime: '2022-06-01',
+    description:
+      'A spy, an assassin and a telepath come together to pose as a family, each for their own reasons, while hiding their true identities from each other.A spy, an assassin and a telepath come together to pose as a family, each for their own reasons, while hiding their true identities from each other.A spy, an assassin and a telepath come together to pose as a family, each for their own reasons, while hiding their true identities from each other.A spy, an assassin and a telepath come together to pose as a family, each for their own reasons, while hiding their true identities from each other.A spy, an assassin and a telepath come together to pose as a family, each for their own reasons, while hiding their true identities from each other.A spy, an assassin and a telepath come together to pose as a family, each for their own reasons, while hiding their true identities from each other',
+    channelTitle: 'Netflix anime series',
+    viewCount: 20000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
 
   return (
     <div className={styles.container}>
@@ -18,7 +31,35 @@ const Video = () => {
         className={styles.modal}
         overlayClassName={styles.overlay}
       >
-        <div>Modal Body</div>
+        <iframe
+          id='player'
+          className={styles.videoPlayer}
+          type='text/html'
+          width='100%'
+          height='390'
+          src={`http://www.youtube.com/embed/${router.query.videoId}?autoplay=1&controls=0&rel=0`}
+          frameBorder='0'
+        ></iframe>
+
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={cls(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={cls(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
