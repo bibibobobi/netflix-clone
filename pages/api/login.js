e1ab560971fbb4f880828dc49b1257ad284f38be
bibibobobi <smilingcat33@gmail.com>
2022-07-03 10:05:39 +0800

    create JWT token

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,5 @@
 import { magicAdmin } from '../../lib/magic';
+import jwt from 'jsonwebtoken';
 
 export default async function login(req, res) {
   if (req.method === 'POST') {
@@ -9,7 +10,24 @@ export default async function login(req, res) {
 
       const metadata = await magicAdmin.users.getMetadataByToken(didToken);
       console.log({ metadata });
-      //invoke magic
+
+      // create jwt
+
+      const token = jwt.sign(
+        {
+          ...metadata,
+          iat: Math.floor(Date.now() / 1000),
+          exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+          'https://hasura.io/jwt/claims': {
+            'x-hasura-allowed-roles': ['user', 'admin'],
+            'x-hasura-default-role': 'user',
+            'x-hasura-user-id': `${metadata.issuer}`,
+          },
+        },
+        'thisisasecretthisisasecret34545rsffgdsdwyyffff'
+      );
+      console.log({ token });
+
       res.send({ done: true });
     } catch (error) {
       console.log('Something went wrong logging in', error);
