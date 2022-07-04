dddfdc6c16bf45724f0ebf8517eaaab09e80839a
bibibobobi <smilingcat33@gmail.com>
2022-07-04 20:11:47 +0800

    build stats API and get statistics from userId and videoId

---
+++ pages/api/stats.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import { findVideoIdByUser } from '../../lib/db/hasura';
+
+export default async function stats(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const token = req.cookies.token;
+      if (!token) {
+        res.status(403).send({});
+      } else {
+        const videoId = req.query.videoId;
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        const userId = decodedToken.issuer;
+        const findVideoId = await findVideoIdByUser(token, userId, videoId);
+        res.send({ msg: 'it works', decodedToken, findVideoId });
+      }
+    } catch (error) {
+      console.log('Error occurred /stats', error);
+      res.status(500).send({ done: false, error: error?.message });
+    }
+  }
+}
